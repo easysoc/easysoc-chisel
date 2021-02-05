c7df6f96e1de0170faffe05aeff27fbb68d77ec4
Xinjun Ma <xinjun.ma@qq.com>
2021-02-05 19:27:36 +0800

    Disable support for creating Chisel module

--- src/main/java/org/easysoc/plugins/chisel/module/ChiselModuleBuilder.java
+++ src/main/java/org/easysoc/plugins/chisel/module/ChiselModuleBuilder.java
@@ -1,12 +1,19 @@
 package org.easysoc.plugins.chisel.module;
 
+import com.intellij.ide.projectWizard.NewProjectWizard;
 import com.intellij.ide.util.projectWizard.*;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
 import com.intellij.openapi.util.Disposer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JLabel;
+import javax.swing.JComponent;
+import javax.swing.SwingConstants;
 
 public class ChiselModuleBuilder extends ModuleBuilder {
 
@@ -36,21 +43,51 @@ public class ChiselModuleBuilder extends ModuleBuilder {
     return step;
   }
 
+  @Override
+  public boolean isSuitableSdkType(SdkTypeId sdk) {
+    return sdk == JavaSdk.getInstance();
+  }
+
   // fix setTemplatesList in ProjectTypeStep
   @Override
   public @Nullable ModuleWizardStep getCustomOptionsStep(WizardContext context, Disposable parentDisposable) {
-    ChiselTemplateWizardStep step = null;
-    try {
-      step = new ChiselTemplateWizardStep(context,mySettingsStep);
-    } catch (Exception e) {
-      e.printStackTrace();
+    if (context.isCreatingNewProject()) {
+      ChiselTemplateWizardStep step = null;
+      try {
+        step = new ChiselTemplateWizardStep(context,mySettingsStep);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      Disposer.register(parentDisposable, step);
+      return step;
+    } else {
+
+      return new ModuleWizardStep() {
+        // 不会执行这里的 _init 和 onStepLeaving 方法
+
+        @Override
+        public JComponent getComponent() {
+          JLabel label = new JLabel("Does not support create Chisel module!");
+          label.setHorizontalAlignment(SwingConstants.CENTER);
+          return label;
+        }
+
+        // will call this method after click Next button
+        @Override
+        public void updateDataModel() {
+          NewProjectWizard myWizard = (NewProjectWizard)context.getWizard();
+          myWizard.doCancelAction();
+        }
+      };
     }
-    Disposer.register(parentDisposable, step);
-    return step;
   }
 
   @Override
   public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext, @NotNull ModulesProvider modulesProvider) {
-    return new ModuleWizardStep[]{new ChiselModuleSettingStep(wizardContext)};
+    if (wizardContext.isCreatingNewProject()) {
+      return new ModuleWizardStep[]{new ChiselModuleSettingStep(wizardContext)};
+    } else {
+      return new ModuleWizardStep[0];
+    }
   }
 }
